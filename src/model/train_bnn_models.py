7c04aabc53c5afa9875253d7ebb41b7a27bfbf1e
thecml <chr1000@gmail.com>
2024-04-28 19:52:43 -0600

    Set n_epochs to 1000

--- src/model/train_bnn_models.py
+++ src/model/train_bnn_models.py
@@ -44,7 +44,7 @@ training_results, test_results = pd.DataFrame(), pd.DataFrame()
 
 DATASETS = ["SUPPORT", "SEER", "METABRIC", "MIMIC"]
 MODELS = ["mlp", "sngp", "vi", "mcd1", "mcd2", "mcd3"]
-N_EPOCHS = 100
+N_EPOCHS = 1000
 
 tf.config.set_visible_devices([], 'GPU') # use CPU
 
